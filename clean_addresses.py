3d43baa1cf3c092c8e1c7b51c74b799cfd1aa9ea
Pratheek Rebala <pratheekrebala@gmail.com>
2017-01-07 11:31:39 +0530

    F.M.L. - 8hrs of work down the drain.
    Silverlining though - script is much much faster now.

--- clean_addresses.py
+++ clean_addresses.py
@@ -3,13 +3,13 @@ import glob
 from os.path import basename
 import pandas as pd
 import numpy as np
-from postal.expand import expand_address
-from postal.parser import parse_address
+# from postal.expand import expand_address
+# from postal.parser import parse_address
 import logging
 # import zipcode
 # from uszipcode import ZipcodeSearchEngine
 # from pyzipcode import Pyzipcode as pz
-import dask.dataframe as dd
+# import dask.dataframe as dd
 import multiprocessing as mp
 import os
 
@@ -51,39 +51,40 @@ acris_datasets = {}
 def clean_acris_csv(filename):
         # global search
         # search = ZipcodeSearchEngine()
-        zipcodes = pd.read_csv('./data/zip_code_database.csv', encoding='latin-1', index_col='zip')
-
+        zipcodes = pd.read_csv('./data/zip_code_database.csv', encoding='latin-1', dtype=object, converters={'zip': str})
         curr_row_progress = 0
         logger.debug('Starting to process %s', filename)
-        t = pd.read_csv(filename, chunksize=chunksize, iterator=True, dtype=object, converters={'ZIP': str}, 
+        t = pd.read_csv(filename, chunksize=chunksize, iterator=False, dtype=object, converters={'ZIP': str}, 
         header=None, names=['DOCUMENT ID','RECORD TYPE','PARTY TYPE','NAME','ADDRESS 1','ADDRESS 2','COUNTRY','CITY','STATE','ZIP','GOOD THROUGH DATE'])
         #t = dd.read_csv('./data/parsed_sample/real_property_parties_out.csv', blocksize=chunksize, dtype=object, converters={'ZIP': str})
 
         for chunk in t:
-            address_extract = chunk.loc[:, ['ADDRESS 1', 'ADDRESS 2', 'CITY', 'STATE', 'COUNTRY', 'ZIP']]
-            
-            address_extract = pd.merge(address_extract, zipcodes.loc[:,['state','primary_city']], how='left', left_on='ZIP', right_index=True, sort=True, suffixes=('_x', '_y'), copy=False)
-            
+            address_extract = chunk.copy()
+            address_extract['ZIP'] = address_extract['ZIP'].str[:5]
+
+            zipcode_extract = zipcodes.loc[:,['zip', 'state','primary_city']].copy()
+
+            address_extract = pd.merge(address_extract, zipcode_extract, how='left', left_on='ZIP', right_on='zip', suffixes=('_x', '_y'), copy=True)
+
             #If zip code lookup fails, use default
             address_extract['primary_city'].fillna(value=address_extract['CITY'], inplace=True)
             address_extract['state'].fillna(value=address_extract['STATE'], inplace=True)
-
-            address_extract.drop(['STATE', 'CITY'], axis = 1, inplace = True, errors = 'ignore')
-            address_extract = address_extract.reindex_axis(['ADDRESS 1', 'ADDRESS 2', 'primary_city', 'state', 'COUNTRY', 'ZIP'], axis=1)
-
-            #address_extract['STATE'] = address_extract.apply(get_state, axis=1)
-            #address_extract['CITY'] = address_extract.apply(get_city, axis=1)
-
-            sample = address_extract.apply(lambda x: ', '.join(x.dropna().astype(str).values), axis=1)
-
-            chunk['parsed_address'] = sample.apply(lambda x: expand_address(x, languages=['en'], delete_final_periods=True, delete_acronym_periods=True)[0])
-            chunk['STATE'] = address_extract['state']
-            chunk['CITY'] = address_extract['primary_city']
-
-            chunk.to_csv(out_dir + os.path.basename(filename) + '.csv', index=False, header=False, mode='a', chunksize=chunksize)
+            address_extract.drop(['STATE','CITY'], axis = 1, inplace=True, errors='ignore')
+            address_extract.to_csv(out_dir + os.path.basename(filename) + '.csv', index=False, header=False, mode='a', chunksize=chunksize)
 
             curr_row_progress += chunksize
             logger.debug('%s: Wrote out %s rows', filename, curr_row_progress)
+            # Refactor this if you need addresses again - ignore for now because trying to canonicalize on just state,city,zip,name.
+
+            # address_extract.drop(['STATE', 'CITY'], axis = 1, inplace = True, errors = 'ignore')            
+            # address_extract = address_extract.reindex_axis(['ADDRESS 1', 'ADDRESS 2', 'primary_city', 'state', 'COUNTRY', 'ZIP'], axis=1)
+            #  address_extract['STATE'] = address_extract.apply(get_state, axis=1)
+            # address_extract['CITY'] = address_extract.apply(get_city, axis=1)
+            #sample = address_extract.apply(lambda x: ', '.join(x.dropna().astype(str).values), axis=1)
+            #address_extract['parsed_address'] = sample.apply(lambda x: expand_address(x, languages=['en'], delete_final_periods=True, delete_acronym_periods=True)[0])
+            # chunk_sub['STATE'] = address_extract['state']
+            # chunk_sub['CITY'] = address_extract['primary_city']
+            # print(chunk['ZIP'], chunk['new_City'])
 
 pool = mp.Pool(processes = (mp.cpu_count() - 3))
 if __name__ == '__main__':
